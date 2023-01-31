18eb727ef897a91cba95b5582c1fb4c4839183b6
Oshrey Avraham <oshrey16@gmail.com>
2023-01-31 17:20:19 +0200

    update number of groups in data file

--- create_repos.py
+++ create_repos.py
@@ -5,15 +5,16 @@ import json
 
 # arg1 - number of groups
 print("Creating {} groups..".format(sys.argv[1]))
-groups = int(sys.argv[1])
 
 # Opening JSON file
 f = open('data.json')
 # returns JSON object as a dictionary
 data = json.load(f)
-# Setup git key and organization
+# Setup git key, organization and number of groups
 g = Github(data["key"])
 org = g.get_organization(data["organization"])
+groups = data["number_of_groups"]
+
 
 for i in range(1,groups+1):
     time.sleep(0.5)
